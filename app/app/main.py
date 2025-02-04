a9670ed6874eadb87d8fdb220f8b23958b111073
Syed Khaheera <140519655+khaheeraSyed@users.noreply.github.com>
2025-02-04 14:00:08 +0530

    Update main.py

--- app/app/main.py
+++ app/app/main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from app.routes import pages
 
 app = FastAPI()
+
+# Serve static files from the 'frontend' directory
+app.mount("/frontend", StaticFiles(directory="frontend"), name="frontend")
 
 app.include_router(pages.router)
 
 @app.get("/")
 async def root():
     return {"message": "Welcome to the Facebook Insights Microservice"}
+
